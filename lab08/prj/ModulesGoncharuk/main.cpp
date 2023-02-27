70c74b0efef3a0458b2581e33c9ef4d8ccc7f149
OleksandrGoncharuk <bozhko05aleksandr.com>
2023-02-27 21:32:36 +0300

    До теки prj додано ModulesGoncharuk

---
+++ lab08/prj/ModulesGoncharuk/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+void SoftwareDeveloper(){
+    system("chcp 1251");
+    system("cls");
+
+    setlocale(LC_ALL, "ukr");
+    cout << "\n ------------------------------------------------\n"
+         << "  | Oleksandr Goncharuk |"
+         << "  | Олександр Гончарук |  "
+         << "\n   ----------- © All Rights Reserved ---------------\n\n\n\n";
+}
